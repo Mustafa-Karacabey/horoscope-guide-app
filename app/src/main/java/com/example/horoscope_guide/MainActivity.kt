f0f690da8bec4b93048606804ce12153a5439127
Mustafa KARACABEY <mustafa.karacabey.engineer@gmail.com>
2021-10-13 12:45:48 +0300

    [Created-Adaptor]

--- app/src/main/java/com/example/horoscope_guide/MainActivity.kt
+++ app/src/main/java/com/example/horoscope_guide/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.horoscope_guide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var burclar = arrayOf("Koç", "Boğa", "Yengeç", "Aslan", "Başak", "Terazi", "Akrep", "Yay", "Oğlak", "Kova","balık")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var burclarAdaptor = ArrayAdapter(this, android.R.layout.simple_list_item_1, burclar) //Datayı alıp 1 tek viewa dönüştürür
+
+        burclar_view.adapter = burclarAdaptor
+
+        burclar_view.setOnItemClickListener { adapterView, view, i, l ->
+            var deneme = view as TextView
+            Toast.makeText(this, "${deneme.text.toString()} tıklanılı ve id : ${deneme.id}", LENGTH_LONG)
+            println("Tıklanıldı ${deneme.text.toString()}")
+
+        }
+
     }
 }
