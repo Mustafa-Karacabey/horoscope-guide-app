f54ede276da6846f1bb6f606905141c5470f3d15
Mustafa KARACABEY <mustafa.karacabey.engineer@gmail.com>
2021-10-13 17:53:22 +0300

    [Added-BaseAdapter]

--- app/src/main/java/com/example/horoscope_guide/myArrayAdapter.kt
+++ app/src/main/java/com/example/horoscope_guide/myArrayAdapter.kt
@@ -8,15 +8,19 @@ import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main_burc.view.*
 
 
+//Bu da bir yöntem fakat asıl yöntem BaseAdaptör ile kişiselleştirme
 class myArrayAdapter(context: Context, resource: Int, textViewResourceId: Int, var  burcAdlari : Array<String>, var  burcTarihleri : Array<String>, var burcResimleri : Array<Int>) : ArrayAdapter<String>(context, resource, textViewResourceId, burcAdlari){
 
 
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
-        var inflater = LayoutInflater.from(context)
+        //convertView Propertisini araştır ve kodu optimize et
 
+        var inflater = LayoutInflater.from(context)
         var view = inflater.inflate(R.layout.activity_main_burc,parent, false)
+
+        //Burayı da başka classa atayıp optimize et
 
         var burc_resmi = view.burc_image
         var burc_adi = view.burc_adi
---
+++ app/src/main/java/com/example/horoscope_guide/myBaseAdaptor.kt
@@ -0,0 +1,6 @@
+package com.example.horoscope_guide
+
+import android.widget.BaseAdapter
+
+class myBaseAdaptor { //BaseAdaptore göre implemenatasyon videoda var ben reclycerdaptor kullanıcam
+}
